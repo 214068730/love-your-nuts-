d65fa577e5561dab2ac3d365958d7625d5dcf134
SiraajW <SiraajW@SiraajW-Lt02.Saratoga.local>
2020-01-31 11:31:00 +0200

    Websockets &controllers configuration.

--- love-your-nuts/src/main/java/com/love_your_nuts/love_your_nuts/App.java
+++ love-your-nuts/src/main/java/com/love_your_nuts/love_your_nuts/App.java
@@ -1,13 +1,20 @@
 package com.love_your_nuts.love_your_nuts;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
+        System.out.println( "LOVE-YOUR-NUTS" );
     }
 }
