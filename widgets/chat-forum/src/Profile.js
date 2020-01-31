3058e81d0019472d58b002c5f11672ccacd9e1d5
Lance Kuttner <lancek@saratoga.co.za>
2020-01-31 16:03:26 +0200

    Added story to profile

--- widgets/chat-forum/src/Profile.js
+++ widgets/chat-forum/src/Profile.js
@@ -8,7 +8,11 @@ const Profile = (props) => {
   return (
     <>
       <img src='https://pbs.twimg.com/profile_images/914451216687681536/KB9O1oqf_400x400.jpg' />
+      <br />
       <div> Name: {props.match.params.user}</div>
+      <br />
+      <div> This is my story: 
+        I was a happy health nutter and then BOOM. Went to the doc. Met the nuts guys. They helped me big time. I am still alive</div>
     </>
   )
 }
