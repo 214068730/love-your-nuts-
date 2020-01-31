050fdab9a4ab93170b56438cedaa9c9cbb0883c1
menzim <menzim@saratoga.co.za>
2020-01-31 14:50:13 +0200

    Added user names

--- widgets/chat-forum/src/chatForum.js
+++ widgets/chat-forum/src/chatForum.js
@@ -8,8 +8,9 @@ import * as Stomp from 'stompjs'
 
 const ChatForum = () => {
 
-  const [messages, setMessages] = useState(['test'])
+  const [messages, setMessages] = useState([])
   const [messageInput, setMessageInput] = useState('')
+  const [currentUser, setCurrentUser] = useState('Anon')
 
   const sock = new SockJs('http://192.168.0.126:8080/socket')
   const stompClient = Stomp.over(sock)
@@ -29,16 +30,18 @@ const ChatForum = () => {
     <> 
     {messages.map((message, index) => (
         <div key={index}>
-          <p>{message}</p>
+          <p> {message}</p>
         </div>
     ))}
 
       <textarea id="messageInput" rows='4' cols='50' value={messageInput} onChange={ e => setMessageInput(e.target.value)}>
       </textarea>
       <button onClick={e=> {
-        stompClient.send("/app/send/message" , {}, messageInput) 
+        stompClient.send("/app/send/message" , {}, `${currentUser}:  ${messageInput}`) 
         setMessageInput('')
         }}>POST</button>
+        <br/>
+        <input type='text' value={currentUser} onChange={e => setCurrentUser(e.target.value)}/>
     </>
   )
 }
