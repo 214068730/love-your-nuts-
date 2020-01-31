1ecccee05f55c4973ba487c5f6b6a8093005063c
jesseh <jesseh@saratoga.co.za>
2020-01-31 11:44:45 +0200

    UserRoles complete REST

--- love-your-nuts/src/main/java/com/love_your_nuts/repositories/UserRolesRepo.java
+++ love-your-nuts/src/main/java/com/love_your_nuts/repositories/UserRolesRepo.java
@@ -1,5 +1,13 @@
 package com.love_your_nuts.repositories;
 
-public class UserRolesRepo {
+import org.springframework.data.repository.CrudRepository;
 
+import com.love_your_nuts.entities.UserRoles;
+
+public interface UserRolesRepo extends CrudRepository<UserRoles, Long>  
+{
+	UserRoles findByUserRoleId(Long id);
+	
+	UserRoles findByEmailEmail(String user);
 }
+	
