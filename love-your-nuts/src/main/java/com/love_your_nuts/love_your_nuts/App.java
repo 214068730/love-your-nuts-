8cb37bb9c5798ed8ce202c26c448568651c1aa6a
Siraaj_Wilkinson <siraaj.wilkonson1995@gmail.com>
2020-01-31 10:42:25 +0200

    Add files via upload

---
+++ love-your-nuts/src/main/java/com/love_your_nuts/love_your_nuts/App.java
@@ -0,0 +1,13 @@
+package com.love_your_nuts.love_your_nuts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
