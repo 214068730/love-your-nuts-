da431be97c2c3f683baa0a37283c74b83f682dc7
jesseh <jesseh@saratoga.co.za>
2020-01-31 15:47:43 +0200

    user story

--- love-your-nuts/src/main/resources/data.sql
+++ love-your-nuts/src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO user_roles (email, role) VALUES
   ('test', 'guest'),
   ('admin', 'admin');
   
-  INSERT INTO user (userFirstName, userLastName, userEmail, userAge) VALUES
-  ('admin', 'admin', 'admin', '123'),
-  ('test', 'test', 'test', '123');
+  INSERT INTO user (userFirstName, userLastName, userEmail, userAge, userStory) VALUES
+  ('admin', 'admin', 'admin', '123', 'I had testicular cancer 5 years ago'),
+  ('test', 'test', 'test', '123', 'Ive recently found out I have testicular cancer');
